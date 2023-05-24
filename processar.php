875fb18a496079f4950e6d1461e33409000cb7c5
CatolicaRFB <134514441+CatolicaRFB@users.noreply.github.com>
2023-05-24 10:52:28 -0300

    Create processar.php

---
+++ processar.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Dados pessoais
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    // Arquivo anexado
+    $arquivo = $_FILES['documento'];
+
+    // Verificar se um arquivo foi enviado
+    if ($arquivo['error'] === UPLOAD_ERR_OK) {
+        $nomeArquivo = $arquivo['name'];
+        $caminhoArquivo = 'caminho/para/armazenar/' . $nomeArquivo;
+
+        // Mover o arquivo para o local desejado
+        if (move_uploaded_file($arquivo['tmp_name'], $caminhoArquivo)) {
+            // Arquivo movido com sucesso, você pode realizar outras operações aqui
+
+            // Exemplo: exibir uma mensagem de sucesso e exibir os dados recebidos
+            echo "Formulário enviado com sucesso!<br><br>";
+            echo "Nome: " . $nome . "<br>";
+            echo "E-mail: " . $email . "<br>";
+            echo "Telefone: " . $telefone . "<br>";
+            echo "Arquivo anexado: " . $nomeArquivo . "<br>";
+        } else {
+            echo "Erro ao mover o arquivo para o destino desejado.";
+        }
+    } else {
+        echo "Erro ao enviar o arquivo.";
+    }
+} else {
+    echo "O formulário não foi enviado corretamente.";
+}
+?>
